306f6c039f732cb1b86aa2ad2dcf43c9494ad823
unknown <101507136+andboss25@users.noreply.github.com.>
2024-12-07 18:10:16 +0200

    updated bots

--- demos/cool_command.py
+++ demos/cool_command.py
@@ -1,13 +1,34 @@
 import anticord
 import anticord.AntiCordPlainHttpRequest as AntiCordHttp
 import anticord.AntiCordGateway as AntiCordGateway
+import anticord.AntiCordPlainHttpRequest
 
 BOT = AntiCordHttp.BOT("") # Token here
 Gateway = AntiCordGateway.GatewayWebsocket.CreateGatewayObj(BOT.TOKEN)
-cool_role_id = 1249356732120039465 # Cool role id here
+cool_role_id = 1 # Cool role id here
+
+def DoesGuildMemberHaveRole(guild,role_id:int,user):
+            if guild.GetGuildMember(user.userid)["roles"] == []:
+                return False
+            else:
+                for i in guild.GetGuildMember(user.userid)["roles"]:
+
+                    i = int(i)
+                    if role_id == i:
+                        return True
+                
+                return False
+
+def GetMemberAsInObjectByName(guild,name:str):
+    for i in guild.GetGuildMembers(1000):
+        if i.GetUserAttribute("username") == name:
+            return i
+                
+    return None
 
 Gateway.CreateConnection()
 Gateway.SendIntents(513)
+
 
 while True:
     Event = Gateway.ReciveEvent()
@@ -26,13 +47,12 @@ while True:
                 guild = BOT.GetGuildByID(Event["d"]["guild_id"])
                 channel.SendMessage("Thinking...")
 
-                User = guild.GetMemberAsInObjectByName(text[1])
+                User = GetMemberAsInObjectByName(guild,text[1])
                 
                 channel.SendMessage("Computing coolness factors...")
 
                 
-                if guild.DoesGuildMemberHaveRole(cool_role_id,User) == True:
-                    channel.SendMessage("Yes , " +  User.GetUserName() + " is cool")
+                if DoesGuildMemberHaveRole(guild,cool_role_id,User) == True:
+                    channel.SendMessage("Yes , " +  User.GetUserAttribute("username") + " is cool")
                 else:
                     channel.SendMessage("No he is not")
-
--- demos/moderation_bot.py
+++ demos/moderation_bot.py
@@ -7,8 +7,26 @@ import threading
 BOT = AntiCordHttp.BOT("")
 Gateway = AntiCordGateway.GatewayWebsocket.CreateGatewayObj(BOT.TOKEN)
 
-mod_role_id = 1249356732120039465 # The id of the ppl who can use the commands
+mod_role_id = 1 # The id of the ppl who can use the commands
 
+def DoesGuildMemberHaveRole(guild,role_id:int,user):
+            if guild.GetGuildMember(user.userid)["roles"] == []:
+                return False
+            else:
+                for i in guild.GetGuildMember(user.userid)["roles"]:
+
+                    i = int(i)
+                    if role_id == i:
+                        return True
+                
+                return False
+
+def GetMemberAsInObjectByName(guild,name:str):
+    for i in guild.GetGuildMembers(1000):
+        if i.GetUserAttribute("username") == name:
+            return i
+                
+    return None
 
 Gateway.CreateConnection()
 Gateway.SendIntents(513)
@@ -21,7 +39,7 @@ def kick(Event,guild):
 
             text = text.split()
             try:
-                    kick_user = guild.GetMemberAsInObjectByName(text[1])
+                    kick_user = GetMemberAsInObjectByName(guild,text[1])
             except:
                     channel.SendMessage("No user provided")
                     return 0
@@ -35,9 +53,12 @@ def kick(Event,guild):
             except:
                     reason = "No reason provided"
 
-            kick_user.CreateUserDm().SendMessage("You were kicked in " + guild.GetGuildName() + " for reason: ``" + reason + "``")
-            guild.KickUser(kick_user)
-            channel.SendMessage("Kicked user! (unless they are a higher role than the bot)")
+            kick_user.CreateUserDm().SendMessage("You were kicked in " + guild.GetGuildAttribute("name") + " for reason: ``" + reason + "``")
+            try:
+                guild.KickUser(kick_user)
+                channel.SendMessage("Kicked user! (unless they are a higher role than the bot)")
+            except:
+                channel.SendMessage("Failed to kick")
 
 def ban(Event,guild):
             # One word reason cuz ye
@@ -47,7 +68,7 @@ def ban(Event,guild):
             
             text = text.split()
             try:
-                    kick_user = guild.GetMemberAsInObjectByName(text[1])
+                    kick_user = GetMemberAsInObjectByName(guild,text[1])
             except:
                     channel.SendMessage("No user provided")
                     return 0
@@ -61,9 +82,12 @@ def ban(Event,guild):
             except:
                     reason = "No reason provided"
 
-            kick_user.CreateUserDm().SendMessage("You were banned in " + guild.GetGuildName() + " for reason: ``" + reason + "``")
-            guild.BanUser(kick_user)
-            channel.SendMessage("Banned user! (unless they are a higher role than the bot)")
+            kick_user.CreateUserDm().SendMessage("You were banned in " + guild.GetGuildAttribute("name") + " for reason: ``" + reason + "``")
+            try:
+                guild.BanUser(kick_user)
+                channel.SendMessage("Banned user! (unless they are a higher role than the bot)")
+            except:
+                channel.SendMessage("Failed to ban")
 
 while True:
     Event = Gateway.ReciveEvent()
@@ -74,7 +98,7 @@ while True:
         if Event["t"] == "MESSAGE_CREATE":
             user = BOT.GetUserByID(Event["d"]["author"]["id"])
             guild = BOT.GetGuildByID(Event["d"]["guild_id"])
-            if guild.DoesGuildMemberHaveRole(mod_role_id,user):
+            if DoesGuildMemberHaveRole(guild,mod_role_id,user):
                 if Event["d"]["content"].split()[0] == "!kick":
                     print("Kick command ran!")
                     threading.Thread(target=kick,args=(Event,guild)).start()
